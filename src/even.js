be9bee810e5dffe11ac3684cf97253ae173a26b9
Vano26 <101927822+Vano26@users.noreply.github.com>
2022-04-13 18:15:44 +0300

    Update even.js

--- src/even.js
+++ src/even.js
@@ -2,15 +2,40 @@
 
 import readlineSync from 'readline-sync';
 
-const isEven = () => {
-	console.log('Answer "yes" if the number is even, otherwise answer "no".');
-	console.log('Question: 15')
-	const answer = readlineSync.question('Your answer: ');
-	if (answer === 'no') {
-	  console.log('Correct!');
-	} else {
-	 return console.log(`${answer} is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${userName}!`);
-	}
+const brainEven = () => {
+  // приветствуем игрока
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name?');
+  console.log(`Hello, ${userName}!`);
+
+  // сообщаем правила
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  // функция генерации случайноего числа
+  const randomNum = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+  // функция проверки числа на четность
+  const isEven = (num) => {
+    if (num % 2 === 0) {
+      return 'yes';
+    } return 'no';
+  };
+
+  // первый раунд
+  for (let i = 0; i < 3; i += 1) {
+    const num = randomNum(1, 99); // генерим случайно число
+    const answer = isEven(num); // проверяем первое число на четность
+    console.log(`Question: ${num}`); // задаем вопрос
+    const userAnswer = readlineSync.question('Your answer:'); // получаем ответ игрока
+    if (userAnswer === answer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`);
+      console.log(`Let's try again, ${userName}!`); // отвечаем игроку
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
 };
 
-export default isEven;
+export default brainEven;
