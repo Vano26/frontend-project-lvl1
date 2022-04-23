0d376334d584c1a58a6cec1588c1e3382df1aeaa
Vano26 <antonandreev20202020@gmail.com>
2022-04-23 17:38:13 +0300

    all fix

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
+import readlineSync from 'readline-sync';
 
+export const randomNum = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+export const game = (rule, questionAnswer) => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log(rule);
+
+  for (let i = 0; i < 3; i += 1) {
+    const [question, answer] = questionAnswer();
+    console.log(`Question: ${question}`);
+
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer === answer) {
+      console.log('Correct!');
+    } else {
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${answer}.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+  }
+
+  console.log(`Congratulations, ${userName}!`);
+};
